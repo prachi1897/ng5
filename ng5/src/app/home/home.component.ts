6bee2f6869ca8187256f6abcc40c7ac6dbeb0b6c
prachi1897 <ditprachi@gmail.com>
2019-01-29 15:10:07 +0530

    Adding few animated functionalities and addition and deletion

--- ng5/src/app/home/home.component.ts
+++ ng5/src/app/home/home.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-
+import {trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
+import { getAttrsForDirectiveMatching } from '@angular/compiler/src/render3/view/util';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  //  animations:[
+  //  trigger('goals',[
+  //    transition('* => *',[
+  //      query(':enter', style({ opacity:0 }), {optional:true}),
+  //      query(':enter', stagger('300ms',[
+  //        animate('.6sec  ease-in',keyframes([
+  //         style({opacity: 0,transform: 'translateY(-75%)',offset:0}),
+  //          style({opacity: .5,transform: 'translateY(35px)',offset:.3}),
+  //          style({opacity: 1,transform: 'translateY(0)',offset:1}),
+  //        ]))]),{optional:true}),
+  //        query(':leave', style({ opacity:0 }), {optional:true}),
+  //      query(':leave', stagger('300ms',[
+  //        animate('.6sec  ease-in',keyframes([
+  //         style({opacity: 1,transform: 'translateY(0)',offset:0}),
+  //          style({opacity: .5,transform: 'translateY(35px)',offset:.3}),
+  //          style({opacity: 0,transform: 'translateY(-75%)',offset:1}),
+  //        ]))]),{optional:true})
+
+  //   ])
+  // ])
+
+
+  // ]
 })
 export class HomeComponent implements OnInit {
-  itemCount:number;
+  itemCount:number; 
   btnText:string="Add an item";
   goalText:string="My first life goal";
-  goals=[];
+  goals=['My first life goal','I want to go for scuba diving','I want to hike'];
 
   constructor() { }
 
@@ -21,5 +45,8 @@ export class HomeComponent implements OnInit {
     this.goalText='';
     this.itemCount=this.goals.length;
   }
+removeItem(i){
+  this.goals.splice(i,1);
 
+}
 }
